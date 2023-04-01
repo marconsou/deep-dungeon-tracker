e51d7257c4bfaad7657af7e54332e0569ce3e891
marconsou <marcelocontarinisouza@gmail.com>
2023-04-01 10:14:37 -0300

    Better description

--- DeepDungeonTracker/Window/ConfigurationWindow.cs
+++ DeepDungeonTracker/Window/ConfigurationWindow.cs
@@ -183,9 +183,10 @@ public sealed class ConfigurationWindow : WindowEx, IDisposable
         this.Combo(config.FontType, x => config.FontType = x, "Font");
         this.Combo(config.ScoreCalculationType, x => config.ScoreCalculationType = x, "Score Calculation");
         WindowEx.Tooltip(
-            "Current Floor: Include all floor completion-related score up to the current floor and current character level.\nYou can see your score progressively increasing each time you go to the next floor and level up.\n\n" +
+            "Current Floor: Include all floor completion-related score up to the current floor and current character level.\nYou can see your score progressively increasing each time you go to the next floor or level up.\n\n" +
             "Score Window Floor: Include all floor completion-related score up to the floor where it shows the next score window.\n\n" +
-            "Last Floor: Include all floor completion-related score.");
+            "Last Floor: Include all floor completion-related score at once.\n\n"+
+            "Floor completion-related score has nothing to do with map reveals, and this affects the points earned by killing enemies. It's recommended to change this option before starting a fresh save file.");
         this.DragFloat(config.Scale, x => config.Scale = x, "Scale", 0.01f, 0.25f, 2.0f, "%.2f");
         this.CheckBox(config.IsFlyTextScoreVisible, x => config.IsFlyTextScoreVisible = x, "##IsFlyTextScoreVisible");
         WindowEx.Tooltip("When the score changes, a Fly Text will be shown.");
