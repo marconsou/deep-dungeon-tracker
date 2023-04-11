c7c3ae6e1247e47c807f107acbb5d28954fce380
marconsou <marcelocontarinisouza@gmail.com>
2023-04-11 03:47:05 -0300

    Fix

--- DeepDungeonTracker/Data/BossStatusTimerData.cs
+++ DeepDungeonTracker/Data/BossStatusTimerData.cs
@@ -108,9 +108,6 @@ public class BossStatusTimerData
 
     public void TimerEnd()
     {
-        if (this.Combat.HasEnded())
-            return;
-
         foreach (var item in this.Medicated)
             item.TimerEnd();
 
