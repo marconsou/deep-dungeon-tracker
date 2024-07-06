1f47fa53d23e4ee52fb6b7fb5832dbd825d996c0
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2024-07-06 05:27:13 -0300

    PlaySound warning disabled

--- DeepDungeonTracker/Data/DataAudio.cs
+++ DeepDungeonTracker/Data/DataAudio.cs
@@ -6,5 +6,7 @@ namespace DeepDungeonTracker;
 
 public class DataAudio
 {
+#pragma warning disable CA1822
     public void PlaySound(SoundIndex id) => UIModule.PlaySound(Convert.ToUInt32(id, CultureInfo.InvariantCulture));
+#pragma warning restore CA1822
 }
