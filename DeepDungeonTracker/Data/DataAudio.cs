779c3a38a6c04e93f91d939d527be6a4b381b3b0
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2024-07-06 05:08:21 -0300

    PlaySound fix

--- DeepDungeonTracker/Data/DataAudio.cs
+++ DeepDungeonTracker/Data/DataAudio.cs
@@ -1,14 +1,10 @@
-﻿using System.Runtime.InteropServices;
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using System;
+using System.Globalization;
 
 namespace DeepDungeonTracker;
 
 public class DataAudio
 {
-    private delegate ulong PlaySoundDelegate(byte id, ulong unk1, ulong unk2, ulong unk3);
-
-    private PlaySoundDelegate PlaySound_ { get; }
-
-    public DataAudio() => this.PlaySound_ = Marshal.GetDelegateForFunctionPointer<PlaySoundDelegate>(Service.SigScanner.ScanText("E8 ?? ?? ?? ?? 48 63 45 80"));
-
-    public void PlaySound(SoundIndex id) => this.PlaySound_.Invoke((byte)id, 0, 0, 0);
+    public void PlaySound(SoundIndex id) => UIModule.PlaySound(Convert.ToUInt32(id, CultureInfo.InvariantCulture));
 }
